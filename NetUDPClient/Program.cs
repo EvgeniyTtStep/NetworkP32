2b7381f9b112123e7fc17f0b628f0941e0012749
LukashykYevhenii <lukashyk.yevhenii@chnu.edu.ua>
2025-05-06 16:41:22 +0300

    Lesson5-6

---
+++ NetUDPClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Serialization;
+
+class ProgramClient
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Starting UDP client");
+        UdpClient udpClient = new UdpClient("127.0.0.1", 1111);
+        string message = "Hello from UDP client!";
+        byte[] data = Encoding.ASCII.GetBytes(message);
+        
+        udpClient.Send(data, data.Length);
+
+        IPEndPoint remoteEndPoint = null;
+        byte[] receivedData = udpClient.Receive(ref remoteEndPoint);
+        string messageFromServer = Encoding.ASCII.GetString(receivedData);
+        Console.WriteLine("Відповідь від сервера: " + messageFromServer);
+        
+        udpClient.Close();
+    }
+}
---
+++ NetUDPServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class ProgramServer
+{
+    public static void Main(string[] args)
+    {
+        int port = 1111;
+        //UdpClient udpClient = new UdpClient(port);
+        Socket udpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp);
+        Console.WriteLine("Server started...");
+        
+        EndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, port);
+        udpClient.Bind(ipEndPoint);
+
+        byte[] buffer = new byte[1024];
+        while (true)
+        {
+            //byte[] buffer = udpClient.Receive(ref ipEndPoint);
+            int receiveFrom = udpClient.ReceiveFrom(buffer, ref ipEndPoint);
+            string message = Encoding.ASCII.GetString(buffer);
+            Console.WriteLine("Отримано від клієнта: " + message);
+            
+            string command = "Сервер отримав: " + message;
+            byte[] messageBytes = Encoding.ASCII.GetBytes(command);
+            udpClient.SendTo(messageBytes, ipEndPoint);
+        }
+    }
+}
