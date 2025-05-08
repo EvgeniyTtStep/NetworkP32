792b34570e18cfa9a7fbb8f875f7339b31994088
LukashykYevhenii <lukashyk.yevhenii@chnu.edu.ua>
2025-05-08 20:01:33 +0300

    Lesson9-10 part1

---
+++ TCPClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TCPClientExample
+{
+    public static void Main(string[] args)
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 1111);
+        NetworkStream stream = client.GetStream();
+        
+        string message = "Hello from CLIENT!";
+        byte[] msg = Encoding.ASCII.GetBytes(message);
+        stream.Write(msg, 0, msg.Length);
+        
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Відповідь від сервера: " + response);
+        
+        client.Close();
+        stream.Close();
+        
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+class TCPServer
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("TCP Server");
+
+        TcpListener server = new TcpListener(IPAddress.Any, 1111);
+        server.Start();
+        Console.WriteLine("Server Started");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Client Connected");
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] bytes = new byte[1024];
+            int read = stream.Read(bytes, 0, bytes.Length);
+            string received = System.Text.Encoding.UTF8.GetString(bytes, 0, read);
+            Console.WriteLine("Отримано: " + received);
+            
+            string msg = "Hello from SERVER!!!";
+            byte[] msgBytes = System.Text.Encoding.UTF8.GetBytes(msg);
+            stream.Write(msgBytes, 0, msgBytes.Length);
+            
+            client.Close();
+        }
+        
+        server.Stop();
+    }
+}
