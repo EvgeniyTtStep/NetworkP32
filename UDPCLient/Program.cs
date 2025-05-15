8e532884839b3bb0f4ad2927f2fa47f63039f010
LukashykYevhenii <lukashyk.yevhenii@chnu.edu.ua>
2025-05-15 12:19:11 +0300

    Lesson11-12

---
+++ UDPCLient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Starting UDP client");
+
+        UdpClient udpClient = new UdpClient();
+
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+        udpClient.Connect(endPoint);
+
+
+        Thread thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    IPEndPoint remoteEndPoint = null;
+                    byte[] buffer = udpClient.Receive(ref remoteEndPoint);
+                    string message = Encoding.ASCII.GetString(buffer);
+                    Console.WriteLine("Server: " + message);
+                }
+            }
+        );
+        thread.Start();
+
+        while (true)
+        {
+            string msg = Console.ReadLine();
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            udpClient.Send(data, data.Length);
+        }
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Serialization;
+
+class UDPProgram
+{
+    public static void Main(string[] args)
+    {
+        UdpClient server = new UdpClient(8888);
+        Console.WriteLine("Server running...");
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+        
+        Thread thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    byte[] data = server.Receive(ref endPoint);
+                    string text = Encoding.UTF8.GetString(data);
+                    Console.WriteLine("Client: " + text);
+                }
+            }
+        );
+        thread.Start();
+
+        while (true)
+        {
+            string msg = Console.ReadLine();
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            server.Send(data, data.Length, endPoint);
+        }
+    }
+}
